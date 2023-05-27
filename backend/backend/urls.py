4ba88b4a3dc87df769287f016e410b03230affcc
Tony <savson143@gmail.com>
2023-05-27 23:46:37 +0300

    fixed tests

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -1,9 +1,7 @@
-from django.contrib import admin
 from rest_framework import routers
+from django.contrib import admin
 from django.urls import include, path
-
 from api import views
-
 
 router = routers.DefaultRouter()
 router.register('tasks', views.TaskView, 'task')
